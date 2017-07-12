2558340db080b845302dbae79417a4ea982ab6b2
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-11 21:18:30 -0400

    complete event listener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+var number = 0;
+
+console.log('number, incremented:' + ++number);
+
 var heading1 = document.getElementsByTagName('h1')[0];
 
 heading1.addEventListener('click', myFunction);
 
 function myFunction() {
     var paragraph = document.createElement('p');
-    paragraph.innerHTML = 'This is click number: ';
+    paragraph.innerHTML = 'This is click number: ' + ++number;
     document.body.appendChild(paragraph);
 }
