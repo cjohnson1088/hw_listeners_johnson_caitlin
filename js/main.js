8aeb6d64ad1fa19658c9cffa8fed3e134f5dde9d
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-13 11:45:23 -0400

    add event listener to the submit button

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var submit = document.getElementsByTagName('input')[3];
+var firstName = document.getElementsByTagName('input')[0];
+var lastName = document.getElementsByTagName('input')[1];
+var email = document.getElementsByTagName('input')[2];
+var message = document.getElementsByTagName('textarea')[0];
+
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + email.value);
+    console.log('The message is: ' + message.value);
+});
