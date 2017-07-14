2493d567995ca2f72c4543474c0f6f251449c61f
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-13 23:18:50 -0400

    change color of submit button when clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//This changes the color of the h1 on hover
+
 var heading = document.getElementsByTagName('h1')[0];
 
 heading.addEventListener('mouseenter', function (event) {
@@ -6,4 +8,10 @@ heading.addEventListener('mouseenter', function (event) {
 
 heading.addEventListener('mouseout', function (event) {
     event.target.style.color = 'white';
+});
+
+//This changes the color of the submit button on click
+
+document.getElementsByTagName('input')[2].addEventListener('click', function (event) {
+    event.target.style.backgroundColor = 'teal';
 });
